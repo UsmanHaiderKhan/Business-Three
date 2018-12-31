03c35362aaf242c59022c126250845ee7f94efe4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-12-31 22:33:32 +0500

    UPdate the Script file or added New js file for the 3D slider

--- Bussiness-Three/Bootstrap 4/js/script.js
+++ Bussiness-Three/Bootstrap 4/js/script.js
@@ -51,8 +51,38 @@ $(function () {
             var show_content = content.substr(0, showChar);
             var hide_content = content.substr(showChar, content.length - showChar);
             var html = show_content + '<span class="remaining-content" style="display:block;margin-top:20px"><span>' + hide_content +
-                '</span><a href="" class="morelink read-more mt-30" style="display:block">' + moretext + '</a>'
+                '</span><a href="" class="morelinks read-more mt-30 arrow-outline" style="display:block">' + moretext + '</a>'
                 + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinks").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+$(function () {
+    var showChar = 180;
+    var moretext = "more details";
+    var lesstext = " less details";
+    $('.comments-spaces').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-content"><span>' + hide_content +
+                '</span><a href="" class="morelink more-details arrow-outline link-style mt-20">' + moretext + '</a>'
+                + '</span> ' + '<span>&#x2192;</span>';
             $(this).html(html);
         }
     });
@@ -100,3 +130,38 @@ $(document).ready(function () {
         autoPlay: true
     });
 });
+/*===================== Js Ranks Counter ====================*/
+$(document).ready(function () {
+
+    var counters = $(".counter-value");
+    var countersQuantity = counters.length;
+    var counter = [];
+
+    for (i = 0; i < countersQuantity; i++) {
+        counter[i] = parseInt(counters[i].innerHTML);
+    }
+
+    var count = function (start, value, id) {
+        var localStart = start;
+        setInterval(function () {
+            if (localStart < value) {
+                localStart++;
+                counters[id].innerHTML = localStart;
+            }
+        }, 2);
+    }
+
+    for (j = 0; j < countersQuantity; j++) {
+        count(0, counter[j], j);
+    }
+});
+/*=================== Change Bannner on Click on Banner =================== */
+function change_banner(id) {
+    $('.hello').addClass('d-none');
+    $('#img-' + id).removeClass('d-none');
+
+}
+function change_overlay(id) {
+    $('#overlay-' + id).removeClass('my-overlay');
+
+}
