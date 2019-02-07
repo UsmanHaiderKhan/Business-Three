928ef24c8057c65f6f706404a3086d784e2223e9
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-07 17:50:20 +0500

    update the files structer in the project

---
+++ Bussiness-Three/assets/Bootstrap 4/js/script.js
@@ -0,0 +1,270 @@
+﻿console.log("usman");
+
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav2 = $(".navbar-brand");
+        $nav2.toggleClass('scrolled', $(this).scrollTop() > $nav2.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav3 = $(".navbar-nav");
+        $nav3.toggleClass('scrolled', $(this).scrollTop() > $nav3.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".navbar-light .navbar-nav .nav-link");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".logo1");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+$(function () {
+    $(document).scroll(function () {
+        var $nav4 = $(".navbar-toggler");
+        $nav4.toggleClass('scrolled', $(this).scrollTop() > $nav4.height());
+    });
+});
+
+
+
+/*================== Read More Text ==================*/
+$(function () {
+    var showChar = 250;
+    var moretext = "READ MORE";
+    var lesstext = " READ LESS";
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-contents" style="display:block;margin-top:20px"><span>' + hide_content +
+                '</span><a href="" class="morelinks read-more mt-30 arrow-outline" style="display:block">' + moretext + '</a>'
+                + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinks").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+$(function () {
+    var showChar = 180;
+    var moretext = "more details &nbsp;&nbsp; &#x2192;";
+    var lesstext = " less details &nbsp;&nbsp; &#x2190;";
+    $('.comments-spaces').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-content service-desc"><span>' + hide_content +
+                '</span><a href="" class="morelink more-details arrow-outline link-style mt-20">' + moretext + '</a>'
+                + '</span> ' + '<span></span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelink").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+//Custom
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 3,
+        showItems: 3
+
+    });
+
+});
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Owl Carousel =======================*/
+$(document).ready(function () {
+    $("#testimonial-slider").owlCarousel({
+        items: 1,
+        itemsDesktop: [1000, 1],
+        itemsDesktopSmall: [979, 1],
+        itemsTablet: [768, 1],
+        pagination: false,
+        navigation: true,
+        navigationText: ["", ""],
+        slideSpeed: 1000,
+        autoPlay: true
+    });
+});
+/*===================== Js Ranks Counter ====================*/
+//$(document).ready(function () {
+
+//    var counters = $(".counter-value");
+//    var countersQuantity = counters.length;
+//    var counter = [];
+
+//    for (i = 0; i < countersQuantity; i++) {
+//        counter[i] = parseInt(counters[i].innerHTML);
+//    }
+
+//    var count = function (start, value, id) {
+//        var localStart = start;
+//        setInterval(function () {
+//            if (localStart < value) {
+//                localStart++;
+//                counters[id].innerHTML = localStart;
+//            }
+//        }, 2);
+//    }
+
+//    for (j = 0; j < countersQuantity; j++) {
+//        count(0, counter[j], j);
+//    }
+//});
+/*=================== Change Bannner on Click on Banner =================== */
+function change_banner(id) {
+    $('.hello').addClass('d-none');
+    $('#img-' + id).removeClass('d-none');
+    $('.yes-sir').addClass('chah-overlay');
+    $('#overlay-' + id).removeClass('chah-overlay');
+
+
+}
+//function change_overlay(id) {
+//    debugger
+//}
+/*==================== 3D Slider Java Script ====================*/
+jQuery(document).ready(function () {
+
+    function detect_active() {
+        // get active
+        var get_active = $("#dp-slider .dp_item:first-child").data("class");
+        $("#dp-dots li").removeClass("active");
+        $("#dp-dots li[data-class=" + get_active + "]").addClass("active");
+    }
+    $("#dp-next").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+        detect_active();
+
+    });
+
+    $("#dp-prev").click(function () {
+        var total = $(".dp_item").length;
+        $("#dp-slider .dp_item:last-child").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+
+    $("#dp-dots li").click(function () {
+        $("#dp-dots li").removeClass("active");
+        $(this).addClass("active");
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+    });
+
+
+    $("body").on("click", "#dp-slider .dp_item:not(:first-child)", function () {
+        var get_slide = $(this).attr('data-class');
+        console.log(get_slide);
+        $("#dp-slider .dp_item[data-class=" + get_slide + "]").hide().prependTo("#dp-slider").fadeIn();
+        $.each($('.dp_item'), function (index, dp_item) {
+            $(dp_item).attr('data-position', index + 1);
+        });
+
+        detect_active();
+    });
+});
+/*============= Applying the class on scrolling ==============*/
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    //>=, not <=
+    if (scroll >= 50) {
+        //clearHeader, not clearheader - caps H
+        $("nav").removeClass("pt-45");
+    }
+    if (scroll == 0) {
+        $("nav").addClass("pt-45");
+    }
+});
+/*============= Load Counter Section is loading ==============*/
+var counters = [];
+counters = $(".counter-value");
+var countersQuantity = counters.length;
+console.log(counters + "," + countersQuantity);
+var counter = [];
+
+for (i = 0; i < countersQuantity; i++) {
+    counter[i] = parseInt(counters[i].innerHTML);
+}
+var count = function (start, value, id) {
+    var localStart = start;
+    setInterval(function () {
+        if (localStart < value) {
+            localStart++;
+            counters[id].innerHTML = localStart;
+        }
+    }, 1);
+}
+var cond = true;
+requestScrollAnimation((pos) => {
+    var elAlert = document.getElementById("counter-strike");
+    var isVisible = isScrolledIntoView(elAlert, true);
+
+    if (isVisible) {
+        if (cond) {
+            for (var j = 0; j < countersQuantity; j++) {
+                count(0, counter[j], j);
+            }
+            cond = false;
+        }
+
+
+    }
+});
